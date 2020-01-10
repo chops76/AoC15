249fc89b5eb961ffdd52609848a4c1753e2790a1
Dan Chopskie <danc@agleader.com>
2020-01-10 09:38:29 -0600

    Try this again

--- aoc15-4/src/main.rs
+++ aoc15-4/src/main.rs
@@ -1,3 +1,29 @@
+extern crate crypto;
+
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
 fn main() {
-    println!("Hello, world!");
+    let part1 = false;
+
+    let my_key = "ckczppom";
+    let mut md5calc = Md5::new();
+
+    let mut prefix = "00000";
+    if !part1 {
+        prefix = "000000";
+    }
+
+    for i in 0..std::u64::MAX {
+        md5calc.input(my_key.as_bytes());
+        md5calc.input(i.to_string().as_bytes());
+
+        let hash = md5calc.result_str();
+        if hash.starts_with(prefix) {
+            println!("Found at {} - MD5 is {}", i, hash);
+            break;
+        }
+        
+        md5calc.reset();
+    }
 }
