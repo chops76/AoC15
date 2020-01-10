e6d829655267aebb298baacfd92d08838c02265b
Dan Chopskie <dchopskie@gmail.com>
2020-01-10 16:29:28 -0600

    Fixed part 1

--- aoc15-5/src/main.rs
+++ aoc15-5/src/main.rs
@@ -12,23 +12,29 @@ fn main() {
         let mut double = false;
         let mut vowels = 0;
         let mut bad = false;
-        for i in 0..(ln.len()-1) {
+        for i in 0..(ln.len()) {
             let ch = bytes[i] as char;
-            let ch1 = bytes[i+1] as char;
 
-            if i != ln.len() - 1 && ch == ch1 {
-                double = true;
-            }
             if ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' {
-                    vowels+=1;
-                }
-            if i != ln.len() - 1 && ((ch == 'a' && ch1 == 'b') ||
-                                     (ch == 'c' && ch1 == 'd') ||
-                                     (ch == 'p' && ch1 == 'q') ||
-                                     (ch == 'x' && ch1 == 'y')) {
-                bad = true;
-                break;
+                vowels+=1;
             }
+
+            if i != ln.len() - 1 {
+                let ch1 = bytes[i+1] as char;
+
+                if ch == ch1 {
+                    double = true;
+                }
+    
+                if (ch == 'a' && ch1 == 'b') ||
+                      (ch == 'c' && ch1 == 'd') ||
+                      (ch == 'p' && ch1 == 'q') ||
+                      (ch == 'x' && ch1 == 'y') {
+                    bad = true;
+                    break;
+                }
+            }
+
         }
         if !bad && double && vowels >= 3 {
             good_strings += 1;
