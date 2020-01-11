3cfd2e8bc1e40a44dbac4c63ec1645a064ddcdea
Dan Chopskie <dchopskie@gmail.com>
2020-01-10 19:01:49 -0600

    Part 1 works

---
+++ aoc15-6/src/main.rs
@@ -0,0 +1,73 @@
+use std::io;
+use std::io::prelude::*;
+
+fn points(pointstr: &str)->(u32, u32) {
+    let split = pointstr.split(',');
+    let vec: Vec<&str> = split.collect();
+    let x: u32 = vec[0].parse()
+        .expect("Parse error");
+    let y: u32 = vec[1].parse()
+        .expect("Parse error");
+    return (x, y);
+}
+
+fn main() {
+    let stdin = io::stdin();
+
+    let mut grid: [[bool; 1000]; 1000] = [[false; 1000];1000];
+    for line in stdin.lock().lines() {
+        let ln = line.unwrap();
+        let split = ln.split(' ');
+        let vec: Vec<&str> = split.collect();
+
+        let mut toggle = false;
+        let mut new_state = false;
+        let x_min: usize;
+        let x_max: usize;
+        let y_min: usize;
+        let y_max: usize;
+
+        if vec[0] == "toggle" {
+            let (x1, y1) = points(vec[1]);
+            let (x2, y2) = points(vec[3]);
+            toggle = true;
+            x_min = x1 as usize;
+            x_max = x2 as usize;
+            y_min = y1 as usize;
+            y_max = y2 as usize;
+        } else if vec[1] == "on" {
+            let (x1, y1) = points(vec[2]);
+            let (x2, y2) = points(vec[4]);
+            new_state = true;
+            x_min = x1 as usize;
+            x_max = x2 as usize;
+            y_min = y1 as usize;
+            y_max = y2 as usize;
+        } else {
+            let (x1, y1) = points(vec[2]);
+            let (x2, y2) = points(vec[4]);
+            x_min = x1 as usize;
+            x_max = x2 as usize;
+            y_min = y1 as usize;
+            y_max = y2 as usize;
+        }
+        for i in x_min..x_max+1 {
+            for j in y_min..y_max+1 {
+                if toggle {
+                    grid[i][j] = !grid[i][j];
+                } else {
+                    grid[i][j] = new_state;
+                }
+            }
+        }
+    }
+    let mut count = 0;
+    for i in 0..1000 {
+        for j in 0..1000 {
+            if grid[i][j] {
+                count += 1;
+            }
+        }
+    }
+    println!("Total number of on: {}", count);
+}
